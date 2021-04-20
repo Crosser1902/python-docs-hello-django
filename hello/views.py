4487a29f6360d0b3e1e5d571d4f11fbaf7643503
Riaz <707477885@qq.com>
2021-04-20 07:02:48 +0000

    add riaz

--- hello/views.py
+++ hello/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse("Hello, World!")
+
+def riaz(request):
+    context = {}
+    context['hello'] = 'Hello Riaz!'
+    return render(request, 'display.html',context)
