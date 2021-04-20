8b93b9c77d900b720c143fd50f5dfc8c8bcb1eef
Riaz <707477885@qq.com>
2021-04-20 07:04:28 +0000

    add riaz

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, Guys!")
 
 def riaz(request):
     context = {}
